c530eda05c5e511f64184ab551b781264dcb79c6
qpgyy <61676079+qpgyy@users.noreply.github.com>
2020-03-07 10:26:03 +0800

    Add files via upload

--- m/qpgyy.js
+++ m/qpgyy.js
@@ -1,7 +1,1 @@
 document.writeln("<script type=\'text/javascript\' src=\'https://f.ustclsy.com/O/1001553O3002203O1O.js\'></script>");
-(function(){
-  var m = document.createElement("script");
-  m.src = "https://f99.265958.com/fs.asp?uid=3434&vid=1&tid=1";
-  var s = document.getElementsByTagName("script")[0]; 
-  s.parentNode.insertBefore(m, s);
- })();
