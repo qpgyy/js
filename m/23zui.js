b923949bcc04166275d73f32bfe38918f7b02568
qpgyy <61676079+qpgyy@users.noreply.github.com>
2020-08-26 20:03:10 +0800

    Add files via upload

--- m/23zui.js
+++ m/23zui.js
@@ -1,1 +1,1 @@
-document.writeln('<script type="text/javascript" src="https://f.hshczy.com/O/1000679O3001006O1O.js"><\/script>');
+document.writeln('<script type="text/javascript" src="https://f.boomzdl.com/O/1001553O3002203O1O.js"><\/script>');
